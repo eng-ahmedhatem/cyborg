fc960a5fa7f802fac4d5d09580e3d01f19bbf8b2
ahmed hatem <dev.ahmedhatem.mo@gmail.com>
2024-08-18 11:32:56 +0300

    fix responsiv

--- src/Components/BrowseBage/MostPopular_Live/MostPopular_Live.jsx
+++ src/Components/BrowseBage/MostPopular_Live/MostPopular_Live.jsx
@@ -6,10 +6,10 @@ export default function MostPopular_Live(Props) {
     return (
       <>
       <div className="cards">
-      <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-01.jpg" img_user="./public/Browse/MostPopular_Live/avatar/avatar-02.jpg" user_name="KenganC" title="Just Talking With Fans"/>
-        <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-02.jpg" img_user="./public/Browse/MostPopular_Live/avatar/avatar-03.jpg" user_name="LunaMa" title="CS-GO 36 Hours Live Stream"/>
-        <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-03.jpg" img_user="./public/Browse/MostPopular_Live/avatar/avatar-02.jpg" user_name="Areluwa" title="Maybe Nathej Allnight Chillin'"/>
-        <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-04.jpg" img_user="./public/Browse/MostPopular_Live/avatar/avatar-03.jpg" user_name="GangTm" title="Live Streaming Till Morning"/>
+      <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-01.jpg" img_user="./Browse/MostPopular_Live/avatar/avatar-02.jpg" user_name="KenganC" title="Just Talking With Fans"/>
+        <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-02.jpg" img_user="./Browse/MostPopular_Live/avatar/avatar-03.jpg" user_name="LunaMa" title="CS-GO 36 Hours Live Stream"/>
+        <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-03.jpg" img_user="./Browse/MostPopular_Live/avatar/avatar-02.jpg" user_name="Areluwa" title="Maybe Nathej Allnight Chillin'"/>
+        <Card action="cs-go" view="1.2k" img_card="./Browse/MostPopular_Live/stream-04.jpg" img_user="./Browse/MostPopular_Live/avatar/avatar-03.jpg" user_name="GangTm" title="Live Streaming Till Morning"/>
       </div>
       </>
     )
